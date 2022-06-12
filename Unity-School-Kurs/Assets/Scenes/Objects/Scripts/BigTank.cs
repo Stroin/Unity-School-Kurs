4998ce54009fcf72abe43426572b79549c9cc9f8
MongooseFox <jakstroinski@gmail.com>
2022-06-12 23:21:27 +0200

    main

---
+++ Unity-School-Kurs/Assets/Scenes/Objects/Scripts/BigTank.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigTank : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I can't move and You too, idiot. WHO ON EARTH DESIGNED ME?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity-School-Kurs/Assets/Scenes/Objects/Scripts/Tank1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Yo, biggy boi, when we're go!?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
